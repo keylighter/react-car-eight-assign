ca49c79de3c86467753a3093069a708f9da38654
keylighter <keysuggest@gmail.com>
2021-09-26 01:25:23 +0600

    json-create

---
+++ src/data/cars.js
@@ -0,0 +1,89 @@
+const carsJson = [
+
+    {
+        name: 'Jeep',
+        image: './images/blue-jeep-parking-public-zone.jpg',
+        mileage: 20,
+        price: 100000,
+        size: 'Big'
+    },
+    {
+        name: 'Watkins',
+        image: './images/pexels-arnie-watkins-3156482.jpg',
+        mileage: 25,
+        price: 120000,
+        size: 'Big'
+    },
+    {
+        name: 'Mastang',
+        image: './images/pexels-avinash-patel-544542.jpg',
+        mileage: 10,
+        price: 200000,
+        size: 'Medium'
+    },
+    {
+        name: 'Hammer-Red',
+        image: './images/pexels-brett-sayles-1638459.jpg',
+        mileage: 17,
+        price: 170000,
+        size: 'Big'
+    },
+    {
+        name: 'Mastang-Black',
+        image: './images/pexels-garvin-st-villier-3311574.jpg',
+        mileage: 12,
+        price: 56000,
+        size: 'Medium'
+    },
+    {
+        name: 'Oldy-Goldy',
+        image: './images/pexels-johnmark-smith-253096.jpg',
+        mileage: 26,
+        price: 20000,
+        size: 'Small'
+    },
+    {
+        name: 'Maredeze',
+        image: './images/pexels-mike-1007410.jpg',
+        mileage: 15,
+        price: 100000,
+        size: 'Small'
+    },
+    {
+        name: 'Range-Rover',
+        image: './images/pexels-mike-1035108.jpg',
+        mileage: 18,
+        price: 200000,
+        size: 'Big'
+    },
+    {
+        name: 'Audi',
+        image: './images/pexels-mike-112460.jpg',
+        mileage: 32,
+        price: 120000,
+        size: 'Small'
+    },
+    {
+        name: 'Blue-Posh',
+        image: './images/pexels-mike-116675.jpg',
+        mileage: 22,
+        price: 90000,
+        size: 'Small'
+    },
+    {
+        name: 'White-Audi',
+        image: './images/pexels-mike-120049.jpg',
+        mileage: 23,
+        price: 80000,
+        size: 'Small'
+    },
+    {
+        name: 'Family-Master',
+        image: './images/pexels-mike-1007410.jpg',
+        mileage: 30,
+        price: 100000,
+        size: 'Big'
+    },
+
+
+]
