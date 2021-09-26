358d8718f04b585024b338ab317b0a15e2b86a68
keylighter <keysuggest@gmail.com>
2021-09-26 18:44:37 +0600

    dollar-sign

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <div className='header-section'>
             <h1 >Car Show</h1>
             <h4>Now you got your channce to enhance your CAR COLLECTION!!!</h4>
-            <h4>Your Budget Limit : $ 100 million</h4>
+            <h4>Your Budget Limit : $100 million</h4>
             <nav className='navbar'>
                 <a className='navpoint' href='/shop'>About Us </a>
                 <a className='navpoint' href='/orders'>Blogs</a>
